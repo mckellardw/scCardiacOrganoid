c9e146e13d4e2dfad11cb440c0c549f874b05fcb
mckellardw <dwm269@cornell.edu>
2023-02-09 16:41:42 -0500

    Added cell-cell communication scritps

---
+++ scripts/r/cellchat_helpers.R
@@ -0,0 +1,21 @@
+# Pull pathways out of a list of CellChat objects
+getPathways <- function(
+    chat.list,
+    combo = c("intersection","union")
+    # slot="netP" #TODO
+){
+  path.list <- lapply(
+    chat.list,
+    FUN = function(CHAT) CHAT@netP$pathways
+  )
+  
+  if(combo[1]=="intersection"){
+    return(
+      sort(Reduce(intersect, path.list))
+    )
+  }else if(combo[1]=="union"){
+    return(
+      sort(unique(unlist(path.list)))
+    )
+  }
+}
